9cf38dd8242b2ba02782f897ffa33d82557210aa
Simen Smehaugen <simdevgen@outlook.com>
2023-11-08 15:52:18 +0100

    added prev & next button functionality

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: 'Philanthrope - Fade out',
+        displayName: 'Fade out',
+        artist: 'Philanthrope'
+    },
+    {
+        name: 'Plusma - Parasol',
+        displayName: 'Parasol',
+        artist: 'Plusma'
+    },
+    {
+        name: "Psalm Trees - Its Time to Grow",
+        displayName: "It's Time to Grow",
+        artist: 'Psalm Trees'
+    },
+    {
+        name: 'Ryu Riders - Blue Moon Boys',
+        displayName: 'Blue Moon Boys',
+        artist: 'Ryu Riders'
+    }
+];
 
 // Check if playing
 let isPlaying = false;
@@ -26,3 +53,43 @@ const pauseSong = () => {
 playBtn.addEventListener('click', () => {
     isPlaying ? pauseSong() : playSong()
 });
+
+// Function to add songs to DOM
+const loadSong = song => {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+};
+
+// Current song count
+let songIndex = 0;
+
+// Next Song function
+const nextSong = () => {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    console.log(songIndex)
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Prev Song function
+const prevSong = () => {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    console.log(songIndex)
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On load  - Select a random song
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
